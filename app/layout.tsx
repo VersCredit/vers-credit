50927a7712e2dade6cff9ebf25eb283ee4c16572
vers-credit <verscredit@gmail.com>
2026-03-31 23:19:22 +0530

    fix: move Google Ads script to head for improved loading performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins, Source_Sans_3 } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -35,16 +36,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth no-scrollbar">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9959637639836790"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body
         className={`${sourceSans3.variable} ${poppins.variable} antialiased `}
       >
         {children}
       </body>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9959637639836790"
-        crossOrigin="anonymous"
-      ></script>
     </html>
   );
 }
